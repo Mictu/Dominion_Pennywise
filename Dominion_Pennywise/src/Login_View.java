08d17f41fa652b54b43a24694a115d74103e5426
Pätte <Ptte@LAPTOP-5OP0IF2N.edu.ds.fhnw.ch>
2017-11-07 09:29:52 +0100

    actuall Code

--- Dominion_Pennywise/src/Login_View.java
+++ Dominion_Pennywise/src/Login_View.java
@@ -4,17 +4,7 @@ public class Login_View {
 		System.out.println("sali");
 		
 		System.out.println("DIES IST EIN TESCHT!!");
-		System.out.println("Sojo");
-<<<<<<< HEAD
-		System.out.println("New Sojo"); 
 		
-		System.out.println("new new Sojo");
-=======
-		System.out.println("New Sojo");
-		
-		System.out.println("This here is a new Sysout Line");
-		
->>>>>>> branch 'master' of https://github.com/Mictu/Dominion_Pennywise.git
 	}
 
 }
