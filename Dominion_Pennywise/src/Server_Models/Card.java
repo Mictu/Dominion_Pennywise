d5c07daf3faa83b14719a0fb3d0765d0c44ec63c
Startklar <Startklar@10.207.17.32>
2017-11-08 10:53:22 +0100

    Test2

--- Dominion_Pennywise/src/Server_Models/Card.java
+++ Dominion_Pennywise/src/Server_Models/Card.java
@@ -1,15 +1,17 @@
 package Server_Models;
 
-
 public abstract class Card {
 	private String value;
 	private String name;
 	private int cost;
-
+	
 
 	public void card(String value, String name, int cost) {
 		this.value = value;
 		this.name = name;
 		this.cost = cost;
+		
+		System.out.println("Sojo schreibt");
+		
 	}
 }
