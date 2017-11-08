82417499c6eb8f53e88a3d08e3211329ad0f3447
Yujia <yujia.shi@students.fhnw.ch>
2017-11-08 10:18:12 +0100

    changed main class

--- Dominion_Pennywise/src/Main_Class/Main.java
+++ Dominion_Pennywise/src/Main_Class/Main.java
@@ -3,8 +3,7 @@ package Main_Class;
 public class Main {
 
 	public static void main(String[] args) {
-
-		System.out.println("blalbalaldslafkaslfj");
+		System.out.println("blalbalaldslafdafkaslfj");
 
 	}
 
