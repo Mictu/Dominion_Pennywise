13f0116aa4f4822ba45076bfe928fa76ae5f616e
Pätte <Ptte@LAPTOP-5OP0IF2N.edu.ds.fhnw.ch>
2017-11-08 10:59:16 +0100

    cleaned

--- Dominion_Pennywise/src/Server_Models/Card.java
+++ Dominion_Pennywise/src/Server_Models/Card.java
@@ -1,22 +1,16 @@
 package Server_Models;
 
+
 public abstract class Card {
 	private String value;
 	private String name;
 	private int cost;
-	
+
 
 	public void card(String value, String name, int cost) {
 		this.value = value;
 		this.name = name;
 		this.cost = cost;
-		
-		System.out.println("Sojo schreibt");
-		
-		
-		System.out.println("Puuh");
-		System.out.println("test");
-		
 		
 	}
 }
