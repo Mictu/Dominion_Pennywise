af2c75b3ddce647bd7314a36d39d99d0e19c6374
Mictu <michtu.mt@gmail.com>
2017-11-08 15:35:13 +0100

    Updated class CardDesign_View

--- Dominion_Pennywise/src/Server_Models/Player.java
+++ Dominion_Pennywise/src/Server_Models/Player.java
@@ -1,5 +1,20 @@
 package Server_Models;
 
 public class Player {
-
+	private int point;
+	private int totalTurn = 0;
+	private int money;
+	private int actionPoint;
+	private boolean turn;
+	private int buyPoints;
+	private CardPile deck;
+	
+	protected void player() {
+		
+	}
+	
+	protected void incrementTurn() {
+		totalTurn++;
+	}
+	
 }
--- Dominion_Pennywise/src/Views/CardDesign_View.java
+++ Dominion_Pennywise/src/Views/CardDesign_View.java
@@ -1,5 +1,57 @@
 package Views;
 
-public class CardDesign_View {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class CardDesign_View extends Application {
+
+	// private Button cardBtn;
+	private Stage stage;
+	private Button cardBtn;
+
+	public void cardDesign_View(Button btn) {
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) {
+		Scene scene = new Scene(new Group());
+		stage.setTitle("Button Sample");
+		cardBtn = new Button();
+		
+
+		BorderPane bpane = new BorderPane();
+
+		Image image = new Image(getClass().getResource("/Card_Images/Dorf.jpg").toExternalForm(),300,300,true,true);
+		ImageView view = new ImageView(image);
+
+		cardBtn.setGraphic(view);
+
+		cardBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				System.out.println("card clicked");
+			}
+		});
+
+		bpane.setCenter(cardBtn);
+		((Group) scene.getRoot()).getChildren().add(bpane);
+		scene.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
+		stage.setScene(scene);
+		stage.show();
+		
+	}
 
 }
