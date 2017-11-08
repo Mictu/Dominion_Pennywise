81494a5e2585e2d69a72f19c69fdff024a925520
Mictu <michtu.mt@gmail.com>
2017-11-08 10:32:11 +0100

    add constructor to class card

--- Dominion_Pennywise/src/Server_Models/Card.java
+++ Dominion_Pennywise/src/Server_Models/Card.java
@@ -1,5 +1,16 @@
 package Server_Models;
 
-public class Card {
+public abstract class Card {
+	private String value;
+	private String name;
+	private int cost;
+	
+	
+	public void card(String value, String name, int cost) {
+		this.value = value;
+		this.name = name;
+		this.cost = cost;
+		
+	}
 
 }
