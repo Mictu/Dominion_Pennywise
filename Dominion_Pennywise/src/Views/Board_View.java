301461b49fed605b0fd1e9483eed9f2f3bfab313
Pätte <Ptte@LAPTOP-5OP0IF2N.edu.ds.fhnw.ch>
2017-11-09 14:38:34 +0100

    Added picture to the Board_View

--- Dominion_Pennywise/src/Views/Board_View.java
+++ Dominion_Pennywise/src/Views/Board_View.java
@@ -1,5 +1,57 @@
 package Views;
 
-public class Board_View {
+import Server_Models.Game;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-}
+public class Board_View extends Application{
+
+	private Stage stage;
+//	private Game model;
+	
+	
+	public static void main (String[] args) {
+		launch(args);
+	}
+
+	// Initialize the GUI Content here
+
+	// constructor
+//	protected Board_View(Stage s, Game m) {
+//		this.stage = s;
+//		this.model = m;
+
+		// Set up the GUI in here
+	public void start(Stage stage) {
+		stage.setTitle("Dominion");
+//		stage.setResizable(false);
+		BorderPane root = new BorderPane();
+
+	//SET TOP
+	//SET CENTER
+	//SET RIGHT
+	//SET BOTTOM
+		Button a = new Button();
+		root.setTop(a);
+
+		
+		
+		
+		
+		
+		
+		// SET SCENE
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
+		stage.setScene(scene);
+		
+		stage.show();
+
+	} // Close the Constructor
+	
+}// close class
+
+// Written by Patrick
