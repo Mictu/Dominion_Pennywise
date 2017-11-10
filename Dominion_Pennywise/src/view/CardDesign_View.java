6486986b2f46b05cfab9b58770d3b978e511b10d
Pätte <Ptte@LAPTOP-5OP0IF2N.home>
2017-11-10 20:11:40 +0100

    Added Board GUI

--- Dominion_Pennywise/src/view/CardDesign_View.java
+++ Dominion_Pennywise/src/view/CardDesign_View.java
@@ -10,56 +10,7 @@ public class CardDesign_View {
 	protected Button copperBtn, duchyBtn, estateBtn, funfairBtn, goldBtn, laboratoryBtn;
 	protected Button marketBtn, provinceBtn, silverBtn, smithBtn, villageBtn, woodcutterBtn;
 
-	public void cardDesign_View(Button btn) {
-		
-		copperBtn = new Button();
-		copperBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
-		copperBtn.setId("copper");
-
-		duchyBtn = new Button();
-		duchyBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
-		duchyBtn.setId("duchy");
-
-		estateBtn = new Button();
-		estateBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
-		estateBtn.setId("estate");
-		
-		funfairBtn = new Button();
-		funfairBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
-		funfairBtn.setId("funfair");
-		
-		goldBtn = new Button();
-		goldBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
-		goldBtn.setId("gold");
-		
-		laboratoryBtn = new Button();
-		laboratoryBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
-		laboratoryBtn.setId("laboratory");
-		
-		marketBtn = new Button();
-		marketBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
-		marketBtn.setId("market");
-		
-		provinceBtn = new Button();
-		provinceBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
-		provinceBtn.setId("province");
-		
-		silverBtn = new Button();
-		silverBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
-		silverBtn.setId("silver");
-		
-		smithBtn = new Button();
-		smithBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
-		smithBtn.setId("smith");
-		
-		villageBtn = new Button();
-		villageBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
-		villageBtn.setId("village");
-		
-		woodcutterBtn = new Button();
-		woodcutterBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
-		woodcutterBtn.setId("woodcutter");
-		
+	public CardDesign_View() {
 		
 //		Image image = new Image(getClass().getResource("/Card_Images/Dorf.jpg").toExternalForm(),300,300,true,true);
 //		ImageView view = new ImageView(image);
@@ -67,15 +18,120 @@ public class CardDesign_View {
 //		cardBtn.setGraphic(view);
 		
 		
+		
+//		villageBtn.setOnAction(new EventHandler<ActionEvent>() {
+//			@Override
+//			public void handle(ActionEvent e) {
+//				System.out.println("card clicked");
+//			}
+//		});
+		
+		
 
-		villageBtn.setOnAction(new EventHandler<ActionEvent>() {
+	} // close constructor
+
+	
+	// Getters for every Button (Card)
+	protected Button getCopperBtn() {
+		copperBtn = new Button();
+		copperBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
+		copperBtn.setId("copper");
+		setForAction(copperBtn);
+		return copperBtn;
+	}
+	protected Button getDuchyBtn() {
+		duchyBtn = new Button();
+		duchyBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
+		duchyBtn.setId("duchy");
+		setForAction(duchyBtn);
+		return duchyBtn;
+	}
+	protected Button getEstateBtn() {
+		estateBtn = new Button();
+		estateBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
+		estateBtn.setId("estate");
+		setForAction(estateBtn);
+		return estateBtn;
+	}
+	protected Button getFunfairBtn() {
+		funfairBtn = new Button();
+		funfairBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
+		funfairBtn.setId("funfair");
+		setForAction(funfairBtn);
+		return funfairBtn;
+	}
+	protected Button getGoldBtn() {
+		goldBtn = new Button();
+		goldBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
+		goldBtn.setId("gold");
+		setForAction(goldBtn);
+		return goldBtn;
+	}
+	protected Button getLaboratoryBtn() {
+		laboratoryBtn = new Button();
+		laboratoryBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
+		laboratoryBtn.setId("laboratory");
+		setForAction(laboratoryBtn);
+		return laboratoryBtn;
+	}
+	protected Button getMarketBtn() {
+		marketBtn = new Button();
+		marketBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
+		marketBtn.setId("market");
+		setForAction(marketBtn);
+		return marketBtn;
+	}
+	protected Button getProvinceBtn() {
+		provinceBtn = new Button();
+		provinceBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
+		provinceBtn.setId("province");
+		setForAction(provinceBtn);
+		return provinceBtn;
+	}
+	protected Button getSilverBtn() {
+		silverBtn = new Button();
+		silverBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
+		silverBtn.setId("silver");
+		setForAction(silverBtn);
+		return silverBtn;
+	}
+	protected Button getSmithBtn() {
+		smithBtn = new Button();
+		smithBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
+		smithBtn.setId("smith");
+		setForAction(smithBtn);
+		return smithBtn;
+	}
+	protected Button getVillageBtn() {
+		villageBtn = new Button();
+		villageBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
+		villageBtn.setId("village");
+		setForAction(villageBtn);
+		return villageBtn;
+	}
+	protected Button getWoodcutterBtn() {
+		woodcutterBtn = new Button();
+		woodcutterBtn.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm());
+		woodcutterBtn.setId("woodcutter");
+		setForAction(woodcutterBtn);
+		return woodcutterBtn;
+	}
+	
+	// Activate every created Button (Card)
+	protected void setForAction (Button x) {
+		x.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
-				System.out.println("card clicked");
+				System.out.println(x.getId());
 			}
 		});
-
-		
 	}
+	
+	
+	
+	
+} // close class
 
-}
+
+
+//Written by Michael and Patrick
