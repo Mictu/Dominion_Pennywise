dd8cb649965d7d1916c514fd2b20d1a60dd4867f
Startklar <Startklar@10.207.4.129>
2017-11-13 13:43:05 +0100

    changed views

--- Dominion_Pennywise/src/view/Login_View.java
+++ Dominion_Pennywise/src/view/Login_View.java
@@ -1,30 +1,107 @@
 package view;
 
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
-import server_Models.Game;
+import org.ietf.jgss.GSSName;
 
-public class Login_View {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Login_View extends Application {
 
 	BorderPane mainPane;
 	Stage stage; 
-	Game gamemodel; 
+	GSSName gamemodel; 
 	
-	protected Login_View(Stage stage, Game gamemodel){
-		this.stage = stage; 
-		this.gamemodel = gamemodel; 	
-		stage.setResizable(false);
+	Button lobbyBtn; 
+	Button exitBtn; 
+	Label welcomeLbl; 
+	Label nameLbl; 
+	Label languageLbl; 
 	
-		stage.setTitle("Dominion Login");		
-		mainPane = new BorderPane(); 
-		
-		
-		Scene scene = new Scene(mainPane); 
-		scene.getStylesheets().add(getClass().getResource("Dominion.css").toExternalForm()); 
-		stage.setScene(scene);
-		
+	TextField nameTxtfield; 
+	ComboBox <String> language = new ComboBox<String>(); 
+
+	VBox topvbox = new VBox(); 
+	VBox vbox = new VBox(); //for the center
+	HBox hbox1 = new HBox(); //for name and textfield 
+	HBox hbox2 = new HBox(); //for laguage and dropdown 
+	
+	
+	public static void main (String[] args){
+		launch(args);
 	}
 	
+	public void start(Stage stage){
+//	protected Login_View(Stage stage, Game gamemodel){
+//		this.stage = stage; 
+//		this.gamemodel = gamemodel; 	
+		stage.setResizable(false);
+		stage.setTitle("Dominion Login");		
+	
+		//Hauptpane as Grundgerüst  
+		mainPane = new BorderPane();
+		mainPane.setId("mainPane");
+		
+		mainPane.setCenter(vbox);
+		vbox.setAlignment(Pos.CENTER);
+		vbox.setSpacing(20.00);
+		hbox1.setAlignment(Pos.CENTER);
+		hbox2.setAlignment(Pos.CENTER);
+		
+		
+		
+		//BORDER PANE TOP
+		
+		//Wilkommens Label erstelle nund Top
+		welcomeLbl = new Label("Willkommen"); 
+		welcomeLbl.setId("welcomeLbl");
+		topvbox.getChildren().add(welcomeLbl);
+		topvbox.setAlignment(Pos.CENTER);
+		mainPane.setTop(topvbox);
+		
+		
+		// BORDERPANE CENTER
+		
+		//
+		nameLbl = new Label("Name: "); 
+		nameLbl.setId("nameLbl");
+		languageLbl = new Label("Sprache: "); 
+		languageLbl.setId("languageLbl");
+		nameTxtfield = new TextField(); 
+		
+		
+		language.getSelectionModel().select("Auswählen");
+		language.getItems().addAll("Deutsch","English");
+		
+		hbox1.getChildren().addAll(nameLbl,nameTxtfield); 
+		hbox2.getChildren().addAll(languageLbl, language); 
+		
+		
+		lobbyBtn = new Button("Lobby"); 
+		exitBtn = new Button("Verlassen"); 
+		
+		
+		
+		
+		
+		
+		
+		vbox.getChildren().addAll(hbox1, hbox2,lobbyBtn,exitBtn); 
+		Scene scene = new Scene(mainPane); 
+		scene.getStylesheets().add(getClass().getResource("Login.css").toExternalForm()); 
+		stage.setScene(scene);
+		stage.setFullScreen(true);
+		stage.show();
+		
+	}
+
 
 }
--- Dominion_Pennywise/src/view/Result_View.java
+++ Dominion_Pennywise/src/view/Result_View.java
@@ -1,5 +1,61 @@
 package view;
 
-public class Result_View {
+import org.ietf.jgss.GSSName;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Result_View extends Application{
+	
+	public static void main(String[] args){
+		launch(args); 
+	}
+	
+	
+	BorderPane mainPane; 
+	Stage stage; 
+	GSSName gamemodel; 
+	
+	Label congratulation; 
+	Label youwon; 
+	
+	VBox vbox = new VBox(); 
+	
+	
+	@Override
+	public void start(Stage stage) {
+//	protected Result_View(Stage stage, Game gamemodel){
+//	this.stage = stage, 
+//	this.gamemodel = gamemodel; 	
+	stage.setHeight(600.0);
+	stage.setWidth(400.0);
+	stage.setResizable(false);
+	stage.setTitle("WINNER");
+	mainPane = new BorderPane(); 
+	mainPane.setId("mainPaneResult");
+	
+	
+	
+	congratulation = new Label("CONGRATULATION"); 
+	congratulation.setId("lblresult");
+	youwon = new Label("You Won!!!!");
+	youwon.setId("lblresult");
+	vbox.getChildren().addAll(congratulation, youwon); 
+	vbox.setAlignment(Pos.CENTER);
+	
+	mainPane.setCenter(vbox);
+	
+	Scene scene = new Scene(mainPane); 
+	scene.getStylesheets().add(getClass().getResource("Result.css").toExternalForm());
+	stage.setScene(scene);	
+	stage.show();
+		
+		
+	}
 
 }
