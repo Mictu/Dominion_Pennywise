723ae3377391de92d1de15c7ebfde55aa4532b63
Yujia <yujia.shi@students.fhnw.ch>
2017-11-13 14:04:54 +0100

    fsadf

--- Dominion_Pennywise/src/server/client/Client.java
+++ Dominion_Pennywise/src/server/client/Client.java
@@ -19,7 +19,7 @@ public class Client {
 
 	public void run() {
 		try {
-			socket = new Socket("172.20.10.4", 2303);
+			socket = new Socket("localhost", 2303);
 
 			input = new ObjectInputStream(socket.getInputStream());
 			output = new ObjectOutputStream(socket.getOutputStream());
