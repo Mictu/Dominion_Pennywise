876040f20b67585db1ed4bb2ef39e4e288f9f969
Startklar <Startklar@10.207.21.42>
2017-11-20 15:39:38 +0100

    loggincontroller

--- Dominion_Pennywise/src/controllers/Login_Controller.java
+++ Dominion_Pennywise/src/controllers/Login_Controller.java
@@ -5,12 +5,17 @@ import view.Login_View;
 
 public class Login_Controller {
 	
+	Login_View loginview; 
+	Player player = new Player(); 
 	
-	
-	public Login_Controller(  ){
-
-	
+	public Login_Controller(Login_View loginview  ){
 		
+		this.loginview = loginview; 
+		
+		
+		String text = loginview.nameLbl.getText(); 
+		player.setName(text);
+
 	}
 	
 	
