07818872d60a4475ddb373139edabcd2cdd88087
Startklar <Startklar@10.207.11.109>
2017-12-08 13:39:33 +0100

    resultcontroler

--- Dominion_Pennywise/src/controllers/Result_Controller.java
+++ Dominion_Pennywise/src/controllers/Result_Controller.java
@@ -2,14 +2,20 @@ package controllers;
 
 import server.client.ServerHandler;
 import server_Models.GameLogic;
+import server_Models.Player;
 
 public class Result_Controller {
 	
 	GameLogic gLogic; 
 	
-	public Result_Controller(){
+	public Result_Controller(GameLogic gLogic){
 		
-		gLogic = new GameLogic(); 
+		
+		for(Player p : gLogic.playerList){
+			int minPoint = Integer.MIN_VALUE; 
+			
+		}
+			
 		
 
 		
