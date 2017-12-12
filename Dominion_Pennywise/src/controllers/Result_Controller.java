ac343e6c70e38341c02079c515fb551dffb01a01
Startklar <Startklar@LAPTOP-LP1HF0KU.edu.ds.fhnw.ch>
2017-12-12 12:59:53 +0100

    dga

--- Dominion_Pennywise/src/controllers/Result_Controller.java
+++ Dominion_Pennywise/src/controllers/Result_Controller.java
@@ -4,22 +4,24 @@ import server_Models.Player;
 
 public class Result_Controller {
 	
+	Player winner;
 	
 	public Result_Controller (){
-		
-		
-		for(Player p : Player.player){
-			int minPoint = Integer.MIN_VALUE; 
-			
-		}
-			
-		
-
 		
 		
 		
 	}
 	
+	public Player getWinner(){	
+		for(Player p : Player.player){
+			int minPoint = Integer.MIN_VALUE; 
+			if(p.winPoint > minPoint){
+				minPoint = p.winPoint;
+				winner = p; 
+			}
+		}
+		return winner;	
+	}
 	
 
 }
