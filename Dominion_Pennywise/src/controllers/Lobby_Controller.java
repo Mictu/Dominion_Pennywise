bb10ddec94f3ca34b040310f2a06bf860d5d0715
Mictu <michtu.mt@gmail.com>
2017-12-14 12:47:15 +0100

    added event for pressing enter to send chatmessage

--- Dominion_Pennywise/src/controllers/Lobby_Controller.java
+++ Dominion_Pennywise/src/controllers/Lobby_Controller.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import view.Board_View;
 import view.Lobby_View;
@@ -10,39 +12,43 @@ public class Lobby_Controller {
 	Lobby_View lobbyView;
 	Board_View boardView;
 	Login_View loginView;
-	
-	public Lobby_Controller(Lobby_View lobbyView){
-		this.lobbyView = lobbyView;
 
+	public Lobby_Controller(Lobby_View lobbyView) {
+		this.lobbyView = lobbyView;
 
 		lobbyView.btnStartGame.setOnAction(event -> {
 			Login_Controller.client.sendToServer("start");
 		});
-			
+
 		lobbyView.btnLeaveGame.setOnAction((event) -> {
 			exit(lobbyView.getStage());
 		});
 
 		// Chat send and appenText action
+		lobbyView.txtChatMessage.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+			if (event.getCode() == KeyCode.ENTER) {
+				lobbyView.btnSend.fire();
+			}
+		});
 		lobbyView.btnSend.setOnAction(event -> {
-		Login_Controller.client.sendChatMessage(lobbyView.txtChatMessage.getText());
-		lobbyView.txtChatMessage.clear();
+			Login_Controller.client.sendChatMessage(lobbyView.txtChatMessage.getText());
+			lobbyView.txtChatMessage.clear();
 		});
 
-		Login_Controller.client.newestMessage.addListener((o, oldValue, newValue) -> lobbyView.txtChatArea.appendText(newValue + "\n"));
+		Login_Controller.client.newestMessage
+				.addListener((o, oldValue, newValue) -> lobbyView.txtChatArea.appendText(newValue + "\n"));
 
-		
-//		lobbyView.stage.setOnCloseRequest( event -> client.disconnectClient());
+		// lobbyView.stage.setOnCloseRequest( event -> client.disconnectClient());
 	}// Close Constructor
 
 	public void updateConnectedPlayers(String s) {
 		lobbyView.txtConnectedPlayers.appendText(s + "\n");
 	}
-	
+
 	public void clearConnectedP() {
 		lobbyView.txtConnectedPlayers.clear();
 	}
-	
+
 	// ExitMethode for all Views
 	private void exit(Stage stage) {
 		stage.hide();
