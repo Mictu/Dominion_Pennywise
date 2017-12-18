76b946644a1195126efd6d479452d1607171980b
Startklar <Startklar@LAPTOP-LP1HF0KU.edu.ds.fhnw.ch>
2017-12-18 15:16:36 +0100

    Result View completed

--- Dominion_Pennywise/src/view/Result_View.java
+++ Dominion_Pennywise/src/view/Result_View.java
@@ -5,10 +5,12 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import server_Models.Player;
 
-public class Result_View  {
+public class Result_View {
 
 	BorderPane mainPane;
 	Stage stage;
@@ -17,24 +19,55 @@ public class Result_View  {
 	Label youwon;
 
 	VBox vbox = new VBox();
+	HBox hbox;
+	Label namelbl;
+	Label pointlbl;
 
-	public Result_View(Stage stage) {
-	
+
+		 public Result_View(Stage stage ) {
 
 		
 		stage.setHeight(600.0);//
 		stage.setWidth(400.0);//
-//		stage.setResizable(false);
+		// stage.setResizable(false);
 		stage.setTitle("WINNER");
 		mainPane = new BorderPane();
 		mainPane.setId("mainPaneResult");
 
-		congratulation = new Label("CONGRATULATION");
-		congratulation.setId("lblresult");
-		youwon = new Label("You Won!!!!");
-		youwon.setId("lblresult");
-		vbox.getChildren().addAll(congratulation, youwon);
+		// congratulation = new Label("CONGRATULATION");
+		// congratulation.setId("lblresult");
+		// youwon = new Label("You Won!!!!");
+		// youwon.setId("lblresult");
+		// vbox.getChildren().addAll(congratulation, youwon);
 		vbox.setAlignment(Pos.CENTER);
+		vbox.setId("resultvbox");
+
+		for (int i = 0; i < Player.player.size(); i++) {
+			hbox = new HBox();
+			hbox.setAlignment(Pos.CENTER);
+			hbox.setSpacing(50);
+
+			Label ranglbl = new Label("Platz: " + (i + 1));
+			ranglbl.setId("ranglbl" + (i + 1));
+			if (i >= 2) {
+				ranglbl.setStyle("-fx-text-fill: white;" + "-fx-font-weight: bold;" + "-fx-font-size: 20; ");
+			}
+			namelbl = new Label();
+			namelbl.setText(Player.player.get(i).getName());
+			namelbl.setId("namelbl" + (i + 1));
+			if (i >= 2) {
+				namelbl.setStyle("-fx-text-fill: white;" + "-fx-font-weight: bold;" + "-fx-font-size: 20; ");
+			}
+			pointlbl = new Label();
+			pointlbl.setId("pointlbl" + (i + 1));
+			if (i >= 2) {
+				pointlbl.setStyle("-fx-text-fill: white;" + "-fx-font-weight: bold;" + "-fx-font-size: 20; ");
+			}
+			String point = Integer.toString(Player.player.get(i).winPoint);
+			pointlbl.setText(point);
+			hbox.getChildren().addAll(ranglbl, namelbl, pointlbl);
+			vbox.getChildren().add(hbox);
+		}
 
 		mainPane.setCenter(vbox);
 
@@ -42,18 +75,17 @@ public class Result_View  {
 		scene.getStylesheets().add(getClass().getResource("Result.css").toExternalForm());
 		stage.setScene(scene);
 
+		 }
+		
+	public void start() {
+		stage.setFullScreen(true);
+		stage.show();
+
 	}
 
-	public void start() {
-		stage.setFullScreen(false);
-		stage.show();
-		
-	}
-	
-	public void stop () {
+	public void stop() {
 		stage.hide();
 	}
 
 
-	
 }
