442ee5311ab0b30f41ff6d8fea683d53c6677384
Startklar <Startklar@LAPTOP-LP1HF0KU.edu.ds.fhnw.ch>
2017-12-19 15:38:47 +0100

    Result

--- Dominion_Pennywise/src/controllers/Result_Controller.java
+++ Dominion_Pennywise/src/controllers/Result_Controller.java
@@ -6,18 +6,18 @@ import view.Result_View;
 public class Result_Controller {
 	
 //	Player winner;
-	Result_View resultView; 
+	Result_View resultView;
+	
 	
 	public Result_Controller (Result_View resultView){
 		this.resultView = resultView; 
 		
 		
+		
+		
 	}
 
-	private Result_View resultView() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
 	
 //	public Player getWinner(){	
 //		for(Player p : Player.player){
