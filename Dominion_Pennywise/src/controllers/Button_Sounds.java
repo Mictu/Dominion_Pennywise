7d5feff23d21b6a5839d426086784ebbb1ccdb14
Yujia <yujia.shi@students.fhnw.ch>
2017-12-22 09:26:24 +0100

    javadoc for Button_Sounds and refactored

--- Dominion_Pennywise/src/controllers/Button_Sounds.java
+++ Dominion_Pennywise/src/controllers/Button_Sounds.java
@@ -6,41 +6,52 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 /**
- * Get sounds for the game.
- * 3 sounds exist for different actions.
- * @author Patrick
+ * Add effect sounds to the game 3 different sounds exist for different actions.
+ * 
+ * @author Patrick Ziörjen
  */
 public class Button_Sounds {
-	
+
 	static String musicFileCard = "clicksound.mp3";
 	static Media soundCard = new Media(new File(musicFileCard).toURI().toString());
 
 	static String musicFileSend = "pull-out.mp3";
 	static Media soundSend = new Media(new File(musicFileSend).toURI().toString());
-	
+
 	static String musicFileLogger = "loggersound.mp3";
 	static Media soundLogger = new Media(new File(musicFileLogger).toURI().toString());
 
 	/**
-	 * play a sound if a card is clicked in the Board_View
+	 * Play a specific sound if a card is clicked in the Board_View
+	 * 
+	 * @author Patrick Ziörjen
+	 * @param -
+	 * @return -
 	 */
 	public static void playCardSound() {
 		new MediaPlayer(soundCard).play();
 	}
-	
+
 	/**
-	 * play a sound if a chat - message has been send
+	 * Play a specific sound if the send button is clicked in the Board_View
+	 * 
+	 * @author Patrick Ziörjen
+	 * @param -
+	 * @return -
 	 */
 	public static void playSendSound() {
 		new MediaPlayer(soundSend).play();
 	}
-	
+
 	/**
-	 * play a sound if a notification is shown in the info - box of the Board_View
+	 * Play a specific sound if logger updates himself in the Board_View
+	 * 
+	 * @author Patrick Ziörjen
+	 * @param -
+	 * @return -
 	 */
 	public static void playLoggerSound() {
 		new MediaPlayer(soundLogger).play();
 	}
-	
-	
+
 }
