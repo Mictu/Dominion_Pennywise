957b24fbd89ed109057a2f1042a37946ecedf2ba
Pätte <Ptte@LAPTOP-5OP0IF2N>
2017-12-22 14:22:41 +0100

    comments splash-/serverView

--- Dominion_Pennywise/src/Splash/Server_View.java
+++ Dominion_Pennywise/src/Splash/Server_View.java
@@ -10,100 +10,71 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import server.client.Client_Chat;
-import server.client.Server;
-import view.Lobby_View;
 
+/**
+ * Shows the ServerView in which the intern IP is shown. This IP can be used on
+ * the client - side for a connection to the server in a local network.
+ * 
+ * @author Patrick Ziörjen
+ */
 public class Server_View {
 
-	protected boolean startServer;
-	protected Stage stage;
-	Server server;
-	Lobby_View lobby_view;
+	private Scene scene;
+	private StackPane root;
+	private Stage stage;
+
+	private VBox vB;
 	public TextArea txtClientArea = new TextArea();
 	public Button withServer = new Button("Start Server");
 
 	protected S_View splashScreen;
 
+	/**
+	 * Design of the server-view
+	 * 
+	 * @param stage
+	 *            gets the primaryStage from the server class
+	 */
 	public Server_View(Stage stage) {
 		this.stage = stage;
-		// Server dringend schliessen wenn man einem anderen Spiel beitritt
-		// stage = new Stage();
-		startServer = false;
+		stage.setTitle("Dominion");
+		stage.setResizable(false);
 
-		StackPane root = new StackPane();
+		root = new StackPane();
 		root.setId("root2");
 		root.setPadding(new Insets(40, 40, 40, 40));
-		VBox vB = new VBox(40);
+		vB = new VBox(40);
 		vB.setAlignment(Pos.CENTER);
-
-		// Button withoutServer = new Button("Client");
 
 		vB.getChildren().addAll(withServer, txtClientArea);
 		root.getChildren().add(vB);
 
-		Scene scene = new Scene(root);
+		scene = new Scene(root);
 		scene.getStylesheets().add(getClass().getResource("SplashScreen.css").toExternalForm());
 
 		stage.setScene(scene);
-
-		// stage.setTitle("Dominion");
-		// stage.setResizable(false);
-		// stage.setScene(scene);
-		// stage.show();
-
-		// withServer.setOnAction(event -> {
-		// startServer = true;
-		// withServer.setDisable(true);
-		// server = new Server();
-		// server.connect(server_View);
-		// });
-
-		// withoutServer.setOnAction((event) -> {
-		// startServer = false;
-		// this.stage.hide();
-		// startSplash(primaryStage);
-		// });
-
 	}
 
+	/**
+	 * Opens the stage of the server-view and shows the window
+	 */
 	public void start() {
-		stage.setTitle("Dominion");
-		stage.setResizable(false);
 		stage.show();
 	}
 
-	public Boolean getBoolean() {
-		return this.startServer;
-	}
-
-	public void updateServerView(SimpleStringProperty sString,String string) {
-
+	/**
+	 * Updates the text-area on the server-view. Shows when a client connects to
+	 * the server and the intern IP-adress.
+	 * 
+	 * @param sString
+	 *            newest message from server
+	 * @param string
+	 *            the current information which is shown on the text-area
+	 * @author Michael Tu
+	 */
+	public void updateServerView(SimpleStringProperty sString, String string) {
 		Platform.runLater(() -> {
 			sString.set(string);
 		});
-
 	}
-	
-	public void updateClients() {
-		StringBuffer sb = new StringBuffer();
-		for (Client_Chat c : server.clients) {
-			sb.append(c.toString());
-			sb.append("\n");
-		}
-		Platform.runLater(() -> {
-			txtClientArea.setText(sb.toString());
-		});
-		
-	}
-
-	// public void startSplash(Stage primaryStage) {
-	// try {
-	// splashScreen = new S_View();
-	// splashScreen.run(primaryStage, startServer);
-	// } catch (Exception e) {
-	// System.err.println(e);
-	// }
-	// }
-	//
 }
--- Dominion_Pennywise/src/main_Class/Main.java
+++ Dominion_Pennywise/src/main_Class/Main.java
@@ -1,17 +1,10 @@
 package main_Class;
 
 import Splash.S_View;
-import Splash.Server_View;
-import controllers.Login_Controller;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import server.client.Server;
 import server_Models.Configuration;
 import server_Models.Translator;
-import view.Board_View;
-import view.Lobby_View;
-import view.Login_View;
-import view.Result_View;
 
 /**
  * Main Class of the IT Project Dominion
@@ -30,16 +23,9 @@ import view.Result_View;
  */
 public class Main extends Application {
 
-	Login_View loginView;
-	Lobby_View lobbyView;
-	Board_View boardView;
-	Result_View resultView;
-	Login_Controller loginController;
 	private ServiceLocator serviceLocator;
-
-	S_View splashScreen;
-	Server_View serverView;
-	Server server;
+	private S_View splashScreen;
+	private String language;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -56,11 +42,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		serviceLocator = ServiceLocator.getServiceLocator();
 		serviceLocator.setConfiguration(new Configuration());
-		String language = serviceLocator.getConfiguration().getOption("Language");
+		language = serviceLocator.getConfiguration().getOption("Language");
 		serviceLocator.setTranslator(new Translator(language));
 
 		splashScreen = new S_View();
 		splashScreen.run(primaryStage);
-
 	}
 }
