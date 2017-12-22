05636f5022fb9d528ee2ca55cae7bb665365cc9c
Yujia <yujia.shi@students.fhnw.ch>
2017-12-22 14:22:59 +0100

    javadoc for gamelogic

--- Dominion_Pennywise/src/server_Models/GameLogic.java
+++ Dominion_Pennywise/src/server_Models/GameLogic.java
@@ -6,9 +6,18 @@ import java.util.Collections;
 import server.client.Server;
 import server.client.ServerHandler;
 
+/**
+ * This is a Controller Class. Contains all of the game logic of the game
+ * Dominion. GameLogic.java manages all the commands from Client and Server and
+ * process the game.
+ * 
+ * @author Michael Tu
+ * @author Patrick Ziörjen
+ * @author Sojo Nagaroor
+ * @author Yujia Shi
+ */
 public class GameLogic {
 
-	// Initialize sector here
 	protected CleanUpPhase cleanUpPhase;
 	ActionPhase actionPhase = new ActionPhase();
 	BuyPhase buyPhase = new BuyPhase();
@@ -26,12 +35,18 @@ public class GameLogic {
 	int rounds = 1;
 	boolean firstRound;
 	int finishedCardStack = 0;
-	
+
 	int[] resultPoints = {};
 
 	Player player;
 
-	// Constructor
+	/**
+	 * Constructor for GameLogic.java initialize everything and set points for
+	 * player from the begin of the game.
+	 * 
+	 * @param server
+	 *            - server instance from Server.java
+	 */
 	public GameLogic(Server server) {
 		this.server = server;
 		starter = 0;
@@ -41,56 +56,72 @@ public class GameLogic {
 		for (Player player : Player.player) {
 			gameStart(player);
 		}
-	} // Close Constructor
+	}
 
+	/**
+	 * Generate the start capital of every player and send the generated cards to
+	 * discard from every player
+	 * 
+	 * @author Patrick Ziörjen
+	 * 
+	 * @param player
+	 *            - actual player
+	 */
 	protected void gameStart(Player player) {
 		for (int i = 0; i < START_MONEY; i++) {
-			player.discard.add("copper"); // don't forget to counter++
+			player.discard.add("copper");
 		}
 		for (int i = 0; i < START_ESTATE; i++) {
 			player.discard.add("estate");
 		}
-		
+
 		cleanPhase = new CleanUpPhase(player);
-		
-		
+
 	}
 
+	/**
+	 * Controlls the whole game process Check if the conditions are true to start a
+	 * new game. Command all classes on the server side
+	 * 
+	 * @author Michael Tu
+	 * @author Patrick Ziörjen
+	 * @author Sojo Nagaroor
+	 * @author Yujia Shi
+	 */
 	public void theGame() {
-		if(turn >= Player.player.size()*30 || finishedCardStack >= 3 || buyPhase.pCounter == -1){
+		if (turn >= Player.player.size() * 30 || finishedCardStack >= 3 || buyPhase.pCounter == -1) {
 			sendWinPoints();
 			Collections.sort(Player.player);
 			server.sendToClient("gameover");
 			getSomeTime();
 			sendPointsForResult();
-//			sortPlayersResult();
-		}else{
-		actualPhase = "action";
-		this.player = Player.player.get(index);
-		sendPlayersHand();
-		sendLoggerMessage("name" + this.player.getName());
-		this.player.startRound();
-		sendABMPoints();
-		getSomeTime();
-		int ind = 0;
-		for (Player p : Player.player) {
-			if (p.equals(this.player)) {
-				server.sendStringToClient("action", ind);
-				getSomeTime();
-				sendLoggerMessage("playaction");
-			} else {
-				server.sendStringToClient("cleanup", ind);
-			}
-			ind++;
-		}
-		getSomeTime();
-		if (starter == 0) {
-			sendWinPoints();
+		} else {
+			actualPhase = "action";
+			this.player = Player.player.get(index);
+			sendPlayersHand();
+			sendLoggerMessage("name" + this.player.getName());
+			this.player.startRound();
+			sendABMPoints();
 			getSomeTime();
-			server.sendToClient("rounds"+rounds);
-			starter = 1;
-		}
-		turn++;
+			int ind = 0;
+			for (Player p : Player.player) {
+				if (p.equals(this.player)) {
+					server.sendStringToClient("action", ind);
+					getSomeTime();
+					sendLoggerMessage("playaction");
+				} else {
+					server.sendStringToClient("cleanup", ind);
+				}
+				ind++;
+			}
+			getSomeTime();
+			if (starter == 0) {
+				sendWinPoints();
+				getSomeTime();
+				server.sendToClient("rounds" + rounds);
+				starter = 1;
+			}
+			turn++;
 			if (firstRound) {
 				server.sendToClient(buyPhase.sendRestCards());
 				getSomeTime();
@@ -98,44 +129,18 @@ public class GameLogic {
 			}
 		}
 	}
-	
-	public String sortPlayersResult() {
-		String resultString = "result.";
-		for(Player p : Player.player) {
-			for(int i = 0; i < Player.player.size(); i++) {
-				resultPoints[i] = p.getWinPoints();
-			}
-		}
-		
-		Arrays.sort(resultPoints);
-		
-		for(int i : resultPoints) {
-			System.out.println(i);
-		}
-		
-		//Revers the array
-		for(int i = 0; i < resultPoints.length / 2; i++)
-		{
-		    int temp = resultPoints[i];
-		    resultPoints[i] = resultPoints[resultPoints.length - i - 1];
-		    resultPoints[resultPoints.length - i - 1] = temp;
-		}
-		
-		for(int i : resultPoints) {
-			System.out.println(i);
-		}
-		
-		for(int i : resultPoints) {
-			for(Player pl : Player.player) {
-				if(i == pl.getWinPoints()) {
-					resultString = resultString.concat(pl.getName() + "-" + i + ".");
-				}
-			}
-		}
-		System.out.println(resultString);
-		return resultString;
-	}
 
+	/**
+	 * controlls the whole logic of playing a card in this game. Send logger
+	 * messages if something specific happens
+	 * 
+	 * @author Michael Tu
+	 * @author Sojo Nagaroor
+	 * @author Patrick Ziörjen
+	 * @author Yujia Shi
+	 * 
+	 * @param message
+	 */
 	public void playCard(String message) {
 		switch (actualPhase) {
 		case "action":
@@ -183,17 +188,21 @@ public class GameLogic {
 		return this.index;
 	}
 
-	// get the actual phase to let the client know what cards can be pressed
 	public String getPhase() {
 		return this.actualPhase;
 	}
 
-	// get the player which is playing in this moment
 	public Player getPlayer() {
 		return this.player;
 	}
 
-	// start following phase if button on board view is clicked
+	/**
+	 * handles the endphase butten. if the button is clicked, this method should
+	 * stop the actual phase and start a new phase. at the same time, it refresh the
+	 * cards on the hand
+	 * 
+	 * @author Yujia Shi
+	 */
 	public void endPhase() {
 		switch (actualPhase) {
 		case "action":
@@ -207,10 +216,10 @@ public class GameLogic {
 			cleanUpPhase = new CleanUpPhase(this.player);
 			server.sendStringToClient(actualPhase, index);
 			sendPlayersHand();
-			if(turn % Player.player.size() == 0) {
+			if (turn % Player.player.size() == 0) {
 				rounds++;
 			}
-			server.sendToClient("rounds"+rounds);
+			server.sendToClient("rounds" + rounds);
 			getSomeTime();
 			buyPhase.resetVariablesForBuyPhase(this.player);
 			getIndex();
@@ -218,11 +227,12 @@ public class GameLogic {
 			break; // block this monitor
 		}
 	}
-	
 
-	// This method should actualize the hand on the view (e.g. call it in buy
-	// phase
-	// when u gave away a copper card)
+	/**
+	 * This method actualize the hand and send it to the client
+	 * 
+	 * @author Patrick Ziörjen
+	 */
 	public void sendPlayersHand() {
 		getSomeTime();
 		if (player.hand.isEmpty()) {
@@ -238,6 +248,11 @@ public class GameLogic {
 		getSomeTime();
 	}
 
+	/**
+	 * thread for a short break in the programm to load ressources
+	 * 
+	 * @author Patrick Ziörjen
+	 */
 	public void getSomeTime() {
 		try {
 			Thread.sleep(250);
@@ -254,47 +269,58 @@ public class GameLogic {
 		return this.player;
 	}
 
+	/**
+	 * send the logger message to client
+	 * 
+	 * @author Patrick Ziörjen
+	 * @param loggerMsg
+	 *            - logger message for a specific action from phases
+	 */
 	public void sendLoggerMessage(String loggerMsg) {
 		server.sendToClient("logger" + loggerMsg);
 		getSomeTime();
 	}
 
+	/**
+	 * send the info message to client
+	 * 
+	 * @author Patrick Ziörjen
+	 * @param infoMsg
+	 *            - info message for a specific action from phases
+	 */
 	public void sendInfoMessage(String infoMsg) {
 		server.sendStringToClient("info" + infoMsg, index);
 	}
 
-	public void sendABMPoints() { // action, buy, money
+	/**
+	 * send the ABM Points to the client
+	 * 
+	 * @author Michael Tu
+	 */
+	public void sendABMPoints() {
 		server.sendStringToClient(
 				"abmpoints." + player.getActionPoints() + "." + player.getBuyPoints() + "." + player.getMoney(), index);
 	}
-	
-//	public void sendPointsForResult() {
-//		String result = "";
-//		for(Player p : Player.player){
-//			result = p.getName()+"-"+p.getWinPoints()+".";
-//			for(Player pl : Player.player) {
-//				if(p.winPoint > pl.winPoint) {
-//					result = result + pl.getName()+"-"+pl.getWinPoints()+".";
-//				} else {
-//					result = pl.getName()+"-"+pl.getWinPoints()+"."+result;
-//				}
-//			}
-//		}
-//		System.out.println(result);
-//		server.sendToClient("result.".concat(result));
-//	}
 
-
+	/**
+	 * send the winpoints to client
+	 * 
+	 * @author Michael Tu
+	 */
 	public void sendPointsForResult() {
 		String result = "result.";
-		for(Player p : Player.player) {
+		for (Player p : Player.player) {
 			result = result.concat(p.getName() + "-" + p.getWinPoints() + ".");
 		}
-		System.out.println(result);
 		server.sendToClient(result);
 	}
 
-	
+	/**
+	 * check decks and add winpoints to the correct player
+	 * 
+	 * @author Michael Tu
+	 * @return playersWinPoints - return the acutal winpoints of a player
+	 */
 	public String sendWinPoints() {
 		int countWinP = 0;
 		String playersWinPoints = "winpoints.";
@@ -309,7 +335,7 @@ public class GameLogic {
 				countWinP = addWinPoints(countWinP, card);
 			}
 			p.winPoint = countWinP;
-		
+
 			playersWinPoints = playersWinPoints.concat(p.getName() + "-" + p.winPoint + ".");
 			countWinP = 0;
 		}
@@ -317,6 +343,15 @@ public class GameLogic {
 		return playersWinPoints;
 	}
 
+	/**
+	 * count the points for every card that are important for winpoints
+	 * 
+	 * @author Michael Tu
+	 * @param count
+	 *            - counter for card
+	 * @param card
+	 *            - cardname
+	 */
 	public int addWinPoints(int count, String card) {
 		switch (card) {
 		case "estate":
@@ -333,5 +368,3 @@ public class GameLogic {
 	}
 
 }
-
-// Patrick
